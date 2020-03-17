5e71f5cde330a603870003275d46956f9204286b
shubham <43364183+Shubham3016@users.noreply.github.com>
2020-03-17 18:08:23 +0530

    Add files via upload

---
+++ Random Forest.R
@@ -0,0 +1,18 @@
+install.packages("caret", dependencies = TRUE)
+install.packages("randomForest")
+
+library(randomForest)
+model<-randomForest(iris$Species~.,data=iris,ntree=1000)
+
+# view the forest results
+
+print(model)
+
+# Importance of the variable= Lower Gini
+
+print(importance(model))
+
+# Prediction
+
+pred<-predict(model,iris[,-5])
+table(pred,iris$Species)
